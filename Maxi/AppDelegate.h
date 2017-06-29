f193c3d457990f78b915d550b3c7b254ba08d710
mpkupriyanov <mpkupriyanov@gmail.com>
2017-06-29 18:36:19 +0300

    Initial Commit

---
+++ Maxi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Maxi
+//
+//  Created by mpkupriyanov on 29.06.17.
+//  Copyright © 2017 mpkupriyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Maxi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Maxi
+//
+//  Created by mpkupriyanov on 29.06.17.
+//  Copyright © 2017 mpkupriyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
